965b0882806452316c5582729066a65a4142c07b
sherink08 <sherinkonline@gmail.com>
2019-07-29 05:10:40 +0530

    heroku modifications

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
+// Serve static files....
 app.use(express.static(__dirname + '/dist/gitprofile'));
 
-app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
+// Send all requests to index.html
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/gitprofile/index.html'));
 });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
